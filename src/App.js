78b686bf7d82a7275f177d9a0224e564926254c7
Iva Stanisic <iva.stanisic01@yahoo.com>
2023-01-16 21:38:14 +0100

    Dodata nova ruta search

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import FirstPage from './components/FirstPage';
-import NavBar
- from './components/NavBar';
+import NavBar from './components/NavBar';
+import Pretraga from './components/Pretraga';
+
 function App() {
   return (
     <div className="App">
@@ -11,7 +12,7 @@ function App() {
        <NavBar/>
         <Routes>
           <Route path="/"  element={<FirstPage/>}/>
-
+          <Route path="/search" element={<Pretraga/>}/>
          
         </Routes>
       </Router>
@@ -20,3 +21,4 @@ function App() {
 }
 
 export default App;
+ 
