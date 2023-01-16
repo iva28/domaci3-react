deef113a8d82f788a47894152192cc4c8bea59a7
Iva Stanisic <iva.stanisic01@yahoo.com>
2023-01-16 17:55:23 +0100

    Koriscenjem axiosa ucitani studenti i prosledjeni su iz FirstPage u FormAddStudents

--- src/components/FirstPage.jsx
+++ src/components/FirstPage.jsx
@@ -1,13 +1,24 @@
-import React from 'react'
-import FormAddStudents from './FormAddStudents'
-
+import React from 'react';
+import FormAddStudents from './FormAddStudents';
+import { useEffect,useState } from 'react';
 
 function FirstPage() {
+
+    const [studenti, setStudenti] = useState([]);
+
+    useEffect(()=> {
+        axios.get('/api/students').then(odgovor => {
+            setStudenti(odgovor.data.data);
+        })
+    },[]);
+
+    //console.log(studenti);
+
   return (
     <div className="first-page">
-      <FormAddStudents/>
+      <FormAddStudents  studenti={studenti}/>
     </div>
-  )
+  );
 }
 
-export default FirstPage
+export default FirstPage;
