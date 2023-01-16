e8b286690263777d90863b965c6cd4302b1fdfe3
Iva Stanisic <iva.stanisic01@yahoo.com>
2023-01-16 19:51:37 +0100

    Spread operator za dodavanje novog studenta u niz

--- src/components/FormAddStudents.jsx
+++ src/components/FormAddStudents.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import { useState } from 'react';
 
 function FormAddStudents(props) {
+  //ucitani studenti
+  const[studenti,setStudenti] = useState(props.studenti);
+
 
 const[student,setStudent] = useState({
     ime:'',
@@ -9,22 +12,31 @@ const[student,setStudent] = useState({
     broj_indeksa:'',
 })
 
+console.log(studenti);
+
+function noviStudent(student) {
+  setStudenti(studenti => [...studenti,student]);
+  alert('Uspesno dodat student '+student.ime+'!');
+}
+
+console.log(studenti);
+
   return (
     <div className="form-add-student">
         
         <div className="add-student-fields">
-                <label>Ime: </label>
-                <input type={'text'} className="form-control" value={student.ime} />
+                <label><b>Ime:</b> </label>
+                <input type={'text'} className="form-control" value={student.ime} onChange= {e => setStudent({...student,ime:e.target.value}) }/>
             </div>
             <div className="add-student-fields">
-                <label>Prezime: </label>
-                <input type={'text'} className="form-control" value={student.prezime}  />
+                <label><b>Prezime:</b></label>
+                <input type={'text'} className="form-control" value={student.prezime}  onChange= {e => setStudent({...student,prezime:e.target.value}) }/>
             </div>
             <div className="add-student-fields">
-                <label>Broj indeksa: </label>
-                <input type={'text'} className="form-control" value={student.broj_indeksa}  />
+                <label><b>Broj indeksa:</b> </label>
+                <input type={'text'} className="form-control" value={student.broj_indeksa}  onChange= {e => setStudent({...student,broj_indeksa:e.target.value}) }/>
             </div>
-            <button type="button" className="btn btn-primary" id="dugme_novi">Dodaj</button>
+            <button type="button" className="btn btn-primary" id="dugme_novi" onClick={()=>noviStudent(student)}>Dodaj</button>
             
     </div>
   )
